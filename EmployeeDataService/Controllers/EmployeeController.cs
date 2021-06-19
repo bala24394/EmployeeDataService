c22501e12353219fff1bdd8f8039640e67ae8b78
bala_lucky3 <30320955+bala24394@users.noreply.github.com>
2021-06-19 21:36:34 +0530

    Add project files.

---
+++ EmployeeDataService/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeDataService.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [Authorize]
+        public IEnumerable<Employee> GET()
+        {
+            using (EmployeeEntities entities = new EmployeeEntities())
+            {
+                return entities.Employees.ToList();
+            }
+
+        }
+
+    }
+}
